cd7f985193235fb8890887e999e10f0016fa590e
Grzegorz Kowalski <g.kowalski@uj.edu.pl>
2016-03-24 16:01:14 +0100

    sqlite database support

--- WorktimeLogger.py
+++ WorktimeLogger.py
@@ -3,6 +3,7 @@
 import sys
 import time
 import pynotify
+import sqlite3
 from math import floor
 from PyQt4 import uic, QtGui, QtCore
 
@@ -14,11 +15,70 @@ def sec_to_hm(secs):
 	return (h, m)
 
 
+class Database:
+	def __init__(self, filename="wl.sqlite"):
+		self.db = sqlite3.connect(filename)
+		self.cur = self.db.cursor()
+
+	def commit(self):
+		self.db.commit()
+
+	def close(self):
+		self.db.close()
+
+	def q(self, query):
+		self.cur.execute(query)
+		res = self.cur.fetchall()
+		reslist = []
+		for row in res:
+			row_dict = {}
+			for i, field in enumerate(row):
+				row_dict[self.cur.description[i][0]] = field
+			reslist.append(row_dict)
+		return reslist
+
+	def getLog(self, log_id):
+		res = self.q("SELECT * FROM logs WHERE id == %d" % log_id)
+		return res[0]
+
+	def getActiveLog(self):
+		res = self.q("SELECT * FROM logs WHERE active == 1 ORDER BY id DESC")
+		if not res:			# no active log
+			return self.createNewLog()
+		elif len(res) > 1:	# multiple active logs
+			pass
+		else:
+			return res[0]
+
+	def createNewLog(self):
+		self.q("INSERT INTO logs (active) values (1)")
+		self.commit()
+		return self.getActiveLog()
+
+	def setTimeIn(self, log_id, time):
+		self.q("UPDATE logs SET time_in = %d WHERE id = %d" % (time, log_id))
+		self.commit()
+
+	def setTimeOut(self, log_id, time):
+		self.q("UPDATE logs SET time_out = %d WHERE id = %d" % (time, log_id))
+		self.commit()
+
+	def deactivateLog(self, log_id):
+		self.q("UPDATE logs SET active = 0 WHERE id = %d" % log_id)
+		self.commit()
+
+
 class Log:
 	def __init__(self):
 		pynotify.init("WorktimeLogger")
 		self.t = time.time()
 		self.logged_in = False
+		self.db = Database()
+		self.log = self.db.getActiveLog()
+		if self.log["time_in"]:
+			self.t = self.log["time_in"]
+			self.logged_in = True
+		self.need_new_log = False
 
 	def updateTime(self):
 		self.t = time.time()
@@ -28,25 +88,39 @@ class Log:
 
 	def logIn(self):
 		self.updateTime()
-		self.logged_in = True
+		if self.need_new_log:
+			self.log = self.db.getActiveLog()
+		self.db.setTimeIn(self.log["id"], self.getTime())
+		self.updateDB()
 
+		self.logged_in = True
 		pynotify.Notification("Worktime Logger",
 			"Logged in at %s" % time.strftime(TIMEFMT, time.localtime(self.getTime()))
 		).show()
 
 	def logOut(self):
 		self.updateTime()
-		self.logged_in = False
+		self.db.setTimeOut(self.log["id"], self.getTime())
+		self.db.deactivateLog(self.log["id"])
+		self.updateDB()
 
+		self.logged_in = False
 		pynotify.Notification("Worktime Logger",
 			"Logged out at %s" % time.strftime(TIMEFMT, time.localtime(self.getTime()))
 		).show()
+		self.invalidate()
 
 	def isLoggedIn(self):
 		return self.logged_in
 
 	def close(self):
-		pass # this should close the log file
+		self.db.close()
+
+	def updateDB(self):
+		self.log = self.db.getLog(self.log["id"])
+
+	def invalidate(self):
+		self.need_new_log = True
 
 
 class WLLoginDialog(QtGui.QDialog):
@@ -99,10 +173,12 @@ class WLMain(QtGui.QMainWindow):
 		self.connect(self.LogInButton, QtCore.SIGNAL("clicked()"), self.logIn)
 		self.connect(self.LogOutButton, QtCore.SIGNAL("clicked()"), self.logOut)
 
-		self.connect(self.QuitButton, QtCore.SIGNAL("clicked()"), self.exit)		
+		self.connect(self.QuitButton, QtCore.SIGNAL("clicked()"), self.exit)
 
 		self.log = Log()
-		if not self.log.isLoggedIn():
+		if self.log.isLoggedIn():
+			self.logInGUIAction()
+		else:
 			self.logInPrompt = WLLoginDialog(self)
 
 	def logOut(self):
@@ -118,7 +194,9 @@ class WLMain(QtGui.QMainWindow):
 
 	def logIn(self):
 		self.log.logIn()
+		self.logInGUIAction()
 
+	def logInGUIAction(self):
 		self.LogInButton.setEnabled(False)
 		self.LogOutButton.setEnabled(True)
 
